f2475658a6404bc76a708504a6d9f3dc6a95abe9
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-06-10 22:05:00 +0500

    inheritance task done

---
+++ Back-End Development/Python/Interview prep/Oop/Gpt Task for Oop/Inheritance_Tasks.py
@@ -0,0 +1,45 @@
+# Inheritance
+# Base and Derived Classes:
+# Create a base class Animal with a method make_sound. Create a derived class Dog that overrides make_sound to print "Bark".
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    # Using Super:
+    # Use the super() function in the Dog class to call the __init__ method of the Animal class.
+    def __init__(self, name):
+        super().__init__(name)
+
+    def make_sound(self):
+        return "My dog name is {} and he makes a sound: Bark".format(self.name)
+
+dog_leo = Dog("Leo")
+print(dog_leo.make_sound())
+
+# Multiple Inheritance:
+# Create another base class Pet with an attribute owner_name. Create a class PetDog that inherits from both Dog and Pet.
+print("--------------------------------")
+class Pet:
+    def __init__(self, owner_name):
+        self.owner_name = owner_name
+
+class PetDog(Dog, Pet):
+    def __init__(self, name, owner_name):
+        Dog.__init__(self, name)
+        Pet.__init__(self, owner_name)
+
+    def ownerAndDog(self):
+        return "My name is {} and my dog name is {} and he barks a lot".format(self.owner_name, self.name)
+
+objOfPetDog = PetDog("Leo", "Jack")
+print(objOfPetDog.ownerAndDog())
+
+# Method Resolution Order
+print(PetDog.mro())
+
+
