30c310810b1dd9fe5d0d58643d4c30bbf956122e
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2024-06-13 19:28:09 +0500

    gemini task completed for oop

--- Back-End Development/Python/Interview prep/Oop/OOP.py
+++ Back-End Development/Python/Interview prep/Oop/OOP.py
@@ -22,3 +22,49 @@ print(aayan_house.kitchen())
 farman_house = house()
 print(farman_house.kitchen())
 
+class Book:
+    def __init__(self):
+        self.__title = ""
+        self.__author = ""
+        self.__genre = ""
+        self.__quantity = 0
+
+    def get_title(self):
+        return self.__title
+    
+    def set_title(self,title):
+        self.__title = title
+
+    def get_author(self):
+        return self.__author
+    
+    def set_author(self,author):
+        self.__author = author
+    
+    def get_genre(self):
+        return self.__genre
+    
+    def set_genre(self,genre):
+        self.__genre = genre
+    
+    def get_quantity(self):
+        return self.__quantity
+    
+    def set_quantity(self,quantity):
+        self.__quantity = quantity
+
+    def check_out(self):
+        return "Checking out Book details: {}, {}, {}, {}".format(self.__title, self.__author, self.__genre, self.__quantity) 
+
+    def return_book(self):
+        return "Returning book details: {}, {}, {}, {}".format(self.__title, self.__author, self.__genre, self.__quantity)
+
+
+book = Book()
+book.set_title("World of ifterious")
+book.set_author("Ifham Ahmed Khan")      
+book.set_genre("Science and fantasy")
+book.set_quantity(100)
+
+print(book.check_out())
+print(book.return_book())
