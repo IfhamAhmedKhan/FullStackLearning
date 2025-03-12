38301c748e92487adacbad62835127a447109d2d
Ifham Ahmed Khan <ifham.khan105@gmail.com>
2025-03-12 21:14:58 +0500

    practiceset2 solved

---
+++ Python/Chapter 2/practiceset2.py
@@ -0,0 +1,34 @@
+# Write a python program to add 2 numbers
+a = 10
+b = 20
+print(a+b)
+
+# Write a python program to find remainder when a number is divided by z
+z = 3
+c = 25 % z
+print("Remainder of z is: ",c)
+
+# Check the type of variable assigned using input () function
+x= input("Enter a value")
+print(type(x))
+
+# Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not. Take a = 34 and b = 80
+a = 34
+b = 80
+print(a > b)
+
+# Write a python program to find an average of two numbers entered by the user
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+num3 = (num1+num2)/2
+
+print("Average", num3)
+
+# Write a python program to calculate the square of a number entered by the user.
+
+number = int(input("Enter a number: "))
+squ = number * number
+print("Square:", squ)
+
